0ed08378814f45b275efdb294d38d9b972c77e8e
ac1442AdiTYA <ac1442@srmist.edu.in>
2026-02-16 13:11:57 +0530

    Added Arix

---
+++ OOPSBanner.java
@@ -0,0 +1,11 @@
+public class OOPSBanner {
+public static void main(String[] args) {
+    System.out.println("*************************");
+    System.out.println("*                       *");
+    System.out.println("*   Object-Oriented     *");
+    System.out.println("*   Programming         *");
+    System.out.println("*   in Java            *");
+    System.out.println("*                       *");
+    System.out.println("*************************");
+}
+}

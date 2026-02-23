3b26046a1a70a19321764ca5ac09e923f6e5dfd9
ac1442Aditya <ac1442@srmist.edu.in>
2026-02-23 12:12:35 +0530

    Update Stringjoin.java

--- Stringjoin.java
+++ Stringjoin.java
@@ -1,20 +1,19 @@
 public class Stringjoin {
     public static void main(String[] args) {
-        System.out.println(String.join("\n", new String[] {
-            "   ****    ****   *****   *****   ",
-            "  **  **  **  **  **  **  **      ",
-            " **    ** **    ** **  **   **    ",
-            " **    ** **    ** *****     **** ",
-            " **    ** **    ** ** **        **",
-            "  **  **  **  **  **  **       ** ",
-            "   ****    ****   **  **  *****   ",
-            "                                  ",
-            "      OOOO    PPPP    SSSSS       ",
-            "     OO  OO   PP  PP  SS          ",
-            "     OO  OO   PP  PP   SSS        ",
-            "     OO  OO   PPPP       SSS      ",
-            "     OO  OO   PP            SS    ",
-            "      OOOO    PP       SSSSS      "
-        }));
+        String[] oops = {
+            String.join(" ", "OOOOO", "OOOOO", "PPPPP", "SSSSS"),
+            String.join(" ", "OO OO", "OO OO", "PP PP", "SS"),
+            String.join(" ", "OO OO", "OO OO", "PP PP", "SS"),
+            String.join(" ", "OO OO", "OO OO", "PPPPP", "SSSSS"),
+            String.join(" ", "OO OO", "OO OO", "PP",   "SS"),
+            String.join(" ", "OO OO", "OO OO", "PP",   "SS"),
+            String.join(" ", "OOOOO", "OOOOO", "PP",   "SSSSS"),
+            "",
+            "          OOPS          "
+        };
+
+        for (String line : oops) {
+            System.out.println(line);
+        }
     }
 }

3e1962e736950a031d49694b98a994e05cadbc51
ac1442Aditya <ac1442@srmist.edu.in>
2026-02-23 11:58:53 +0530

    Create Stringjoin.java

---
+++ Stringjoin.java
@@ -0,0 +1,20 @@
+public class Stringjoin {
+    public static void main(String[] args) {
+        System.out.println(String.join("\n", new String[] {
+            "   ****    ****   *****   *****   ",
+            "  **  **  **  **  **  **  **      ",
+            " **    ** **    ** **  **   **    ",
+            " **    ** **    ** *****     **** ",
+            " **    ** **    ** ** **        **",
+            "  **  **  **  **  **  **       ** ",
+            "   ****    ****   **  **  *****   ",
+            "                                  ",
+            "      OOOO    PPPP    SSSSS       ",
+            "     OO  OO   PP  PP  SS          ",
+            "     OO  OO   PP  PP   SSS        ",
+            "     OO  OO   PPPP       SSS      ",
+            "     OO  OO   PP            SS    ",
+            "      OOOO    PP       SSSSS      "
+        }));
+    }
+}
